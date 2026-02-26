6c6232721e129efdcc864a3e4c8e41ae63208c17
Emmy <e.awetobi@gmail.com>
2026-02-26 13:05:40 +0100

    shop disabled

--- app/products/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-    return (
-        <div>
-            <h2 className="text-shadow-neutral-950">Product details</h2>
-        </div>
-    )
-}
